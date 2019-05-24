3186b56393e35fbd6bebd523d52b3c52b09575d1
abunio <18986291675@163.com>
2019-05-24 11:27:49 +0800

    redis缓存数据

--- comocc/src/main/java/com/occ/service/impl/UserServiceImpl.java
+++ comocc/src/main/java/com/occ/service/impl/UserServiceImpl.java
@@ -1,21 +1,41 @@
 package com.occ.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.occ.dao.mapper.UserMapper;
 import com.occ.entity.User;
 import com.occ.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     public List<User> getUsers(User user) {
+        //redisTemplate.delete("users");
+        Object users = redisTemplate.opsForValue().get("users");
+        List<User> list = new ArrayList<User>();
+        if(users==null){
+            //查数据库
+            list = userMapper.selectByUser(user);
 
-        return userMapper.selectByUser(user);
+            redisTemplate.opsForValue().set("users", JSON.toJSON(list),20, TimeUnit.SECONDS);
 
+        }else{
+            //启用缓存
+            list = JSON.parseArray(JSON.toJSONString(users),User.class);
+        }
+
+        return list;
     }
 }
